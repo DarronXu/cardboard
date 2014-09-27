b3eab6d355306e5c1164bf3844914fb6ba1c353c
Administrator <Administrator@xuzizhang-PC>
2014-09-27 17:40:03 -0500

    Till 17:37,
    We learnt to use the API jar properly and created an empty CardboardView
    with it, and we also studied part of the Example Source Code given by
    Google Cardboard Website.
    
    If you would like to see the example source code, first make a commit to
    save all the changes. Then you can checkout the branch called
    "StudyExampleSources". And after that you'll have to checkout the
    "origin" (or "master) branch to switch back to your work.
    
    By Mark.

--- src/com/IYYX/cardboard/Entrance.java
+++ src/com/IYYX/cardboard/Entrance.java
@@ -1,16 +1,30 @@
 package com.IYYX.cardboard;
 
+import javax.microedition.khronos.egl.EGLConfig;
+
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
+import com.google.vrtoolkit.cardboard.CardboardView.StereoRenderer;
+import com.google.vrtoolkit.cardboard.EyeTransform;
+import com.google.vrtoolkit.cardboard.HeadTransform;
+import com.google.vrtoolkit.cardboard.Viewport;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Entrance extends Activity {
+public class Entrance extends CardboardActivity implements StereoRenderer {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_entrance);
+	    CardboardView cardboardView = (CardboardView) findViewById(R.id.cardboardView);
+	    // Associate a CardboardView.StereoRenderer with cardboardView.
+	    cardboardView.setRenderer(this);
+	    // Associate the cardboardView with this activity. 
+	    setCardboardView(cardboardView);
 	}
 
 	@Override
@@ -30,5 +44,41 @@ public class Entrance extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onDrawEye(EyeTransform arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinishFrame(Viewport arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onNewFrame(HeadTransform arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onRendererShutdown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSurfaceChanged(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSurfaceCreated(EGLConfig arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
