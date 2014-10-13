a9844e664ff4d19facc18ea7075f4f486fdc1e62
C4Phone <qq2900726136@gmail.com>
2014-10-12 20:20:41 -0500

    141012-2020 Commit

--- src/com/IYYX/cardboard/MainActivity2.java
+++ src/com/IYYX/cardboard/MainActivity2.java
@@ -3,33 +3,39 @@ package com.IYYX.cardboard;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
+
 import android.app.Activity;
 import android.opengl.*;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity2 extends Activity {
-
-	GLSurfaceView mGLView;
+public class MainActivity2 extends CardboardActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.common_ui);
+        CardboardView cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
+        cardboardView.setRenderer(new CardboardRenderer2(getResources()));
+        setCardboardView(cardboardView);
+        
+		/*
 		mGLView = new GLSurfaceView(this);
 		mGLView.setEGLContextClientVersion(2);		//VERY IMPORTANT!!!
 		mGLView.setRenderer(new Renderer2(getResources()));
-		setContentView(mGLView);
+		setContentView(mGLView);*/
 	}
 	
 	protected void onResume() {
 		super.onResume();
-		mGLView.onResume();
 	}
 	
 	protected void onPause() {
 		super.onPause();
-		mGLView.onPause();
 	}
 
 	@Override
