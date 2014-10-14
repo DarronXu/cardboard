89e8d0d37b3596a51d8a218f2530596eed34d204
C4Phone <qq2900726136@gmail.com>
2014-10-13 22:24:55 -0500

    141013-2216 Big improvements
    This will be the last improvement to our "Simple Triangular" drawings.
    Then we will be mainly focused on Texture Rendering.
    
    Firstly, the readFromObjFile function is optimized, and now it takes
    much less time to load a *.obj file.
    
    Secondly, I created useful functions from some repeatedly called codes.
    
    Thirdly, I created another function to read *.obj file, which can create
    multiple models from one *.obj file. And this is very helpful if we want
    to modularize our project.
    
    I consider it a big improvement, because now, adding a different object
    into scene only requires us to add two or three lines in the code.
    
    By Zhongzhi Yu (Mark)

--- src/com/IYYX/cardboard/CardboardRenderer2.java
+++ src/com/IYYX/cardboard/CardboardRenderer2.java
@@ -2,25 +2,18 @@ package com.IYYX.cardboard;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringBufferInputStream;
-import java.io.StringReader;
 import java.nio.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.opengles.GL10;
 
-import org.apache.http.util.EncodingUtils;
 
-import edu.union.graphics.*;
 import android.app.Activity;
-import android.content.Context;
 import android.content.res.Resources;
 import android.opengl.*;
 import android.os.SystemClock;
 import android.util.Log;
-import android.util.Xml.Encoding;
 
 import com.google.vrtoolkit.cardboard.*;
 public class CardboardRenderer2 implements CardboardView.StereoRenderer {
@@ -89,31 +82,28 @@ public class CardboardRenderer2 implements CardboardView.StereoRenderer {
 
 		GLES20.glEnableVertexAttribArray(mColorHandle);
 		GLES20.glEnableVertexAttribArray(mPositionHandle);
-		
-		//GLES20.glEnableVertexAttribArray(mColorHandle);
 
-		//renderer2_obj.vertices.position(0);
-		//GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, renderer2_obj.fCount*3*3*mBytesPerFloat, renderer2_obj.vertices, GLES20.GL_STATIC_DRAW);
-		//GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, renderer2_obj.fCount*4*mBytesPerFloat, renderer2_obj.colors, GLES20.GL_STATIC_DRAW);
-		
-		renderer2_obj.vertices.position(0);
-		GLES20.glVertexAttribPointer(mPositionHandle, mPositionDataSize, GLES20.GL_FLOAT, false, 0, renderer2_obj.vertices);
-		
-		renderer2_obj.colors.position(0);
-		GLES20.glVertexAttribPointer(mColorHandle, mColorDataSize, GLES20.GL_FLOAT, false, 0, renderer2_obj.colors);
-		
 		GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mMVPMatrix, 0);
-		GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, renderer2_obj.fCount*3);
-		checkGLError("After Draw Array");
+		drawSimpleObjects(earth_obj,mColorHandle,mPositionHandle);
+		for(ObjFile obj:renderer2_obj) drawSimpleObjects(obj,mColorHandle,mPositionHandle);
 	}
-    private static void checkGLError(String func) {
-        int error;
-        while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
-            Log.e("[ZZ]", func + ": glError " + error);
-            throw new RuntimeException(func + ": glError " + error);
-        }
-    }
-
+	
+	/**
+	 * <b>Warning:</b><br/><br/>
+	 * The function does not pass any MVP Matrix to OpenGL.<br/>
+	 * This function can only be used when suitable OpenGL shader exists.<br/>
+	 * @param obj ObjFile object that represent the model to be drawn.
+	 * @param colorHandle Handle to send Color information to OpenGL shader.
+	 * @param positionHandle Handle to send Position information to OpenGL shader.
+	 */
+	void drawSimpleObjects(ObjFile obj, int colorHandle, int positionHandle){
+		obj.vertices.position(0);
+		GLES20.glVertexAttribPointer(positionHandle, mPositionDataSize, GLES20.GL_FLOAT, false, 0, obj.vertices);
+		obj.colors.position(0);
+		GLES20.glVertexAttribPointer(colorHandle, obj.mColorDataSize, GLES20.GL_FLOAT, false, 0, obj.colors);
+		GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, obj.fCount*3);
+	}
+	
 	@Override
 	public void onFinishFrame(Viewport arg0) {
 		// TODO Auto-generated method stub
@@ -142,20 +132,13 @@ public class CardboardRenderer2 implements CardboardView.StereoRenderer {
 	@Override
 	public void onSurfaceChanged(int width, int height) {
 		// TODO Auto-generated method stub
-		oldSurfaceChanged(width,height);
 	}
-
+	
 	@Override
 	public void onSurfaceCreated(EGLConfig arg0) {
-		// TODO Auto-generated method stub
-		oldSurfaceCreated();
-	}
-	
-	//-----------------------OLD CODE-------------------------------
-	
-	public void oldSurfaceCreated() {
 		try {
-			renderer2_obj=this.readCompleteVerticesFromObjFile("renderer2.obj");
+			renderer2_obj=this.readObjFileToArr("renderer2.obj",3);
+			earth_obj=this.readObjFileAsAWhole("earth.obj",3);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -163,7 +146,7 @@ public class CardboardRenderer2 implements CardboardView.StereoRenderer {
 		
 		GLES20.glClearColor(0.3f, 0.3f, 0.3f, 0.5f);
 		GLES20.glEnable(GLES20.GL_DEPTH_TEST);
-		GLES20.glDepthFunc(GLES20.GL_LESS);
+		GLES20.glDepthFunc(GLES20.GL_LESS);				//IMPORTANT!!
 		
 		//---------------Set up View Matrix-----------------
 		// eye: Position the eye behind the origin
@@ -230,182 +213,206 @@ public class CardboardRenderer2 implements CardboardView.StereoRenderer {
 	}
 	
 	int mGLProgramHandle;
-
-	public void oldSurfaceChanged(int width, int height) {
-		//---------------Set up Projection Matrix-------------
-		final float ratio=(float) width/height;
-	}
 	
 	float angleInDegrees;
+
+	ObjFile[] renderer2_obj=null;
+	ObjFile earth_obj=null;
 	
-	ObjFile renderer2_obj=null;
-	
-	/** Allocate storage for the final combined matrix. This will be passed into the shader program. */
 	private float[] mMVPMatrix = new float[16];
-	/** How many elements per vertex. */
-	private final int mStrideBytes = 7 * mBytesPerFloat;
-	/** Offset of the position data. */
-	private final int mPositionOffset = 0;
-	/** Size of the position data in elements. */
 	private final int mPositionDataSize = 3;
-	/** Offset of the color data. */
-	private final int mColorOffset = 3;
-	/** Size of the color data in elements. */
-	private final int mColorDataSize = 4; //or 3 if RGB
 	
-	class ObjFile{
-		public FloatBuffer vertices,normals,elements,colors;
-		public int vCount,fCount;
+	class ObjFile {
+		public String name = "(default)";
+		public boolean hasTextureUV=false;
+		public FloatBuffer vertices, textureUVs, colors;
+		public int mColorDataSize, vCount = 0, fCount = 0;
 	}
 	
-	ObjFile readCompleteVerticesFromObjFile(String assetsName) throws IOException{
+	/**
+	 * @param assetsName
+	 * @param colorDataSize
+	 *   This value can be 3 or 4.<br/>
+	 *   If it's 3, we use RGB, or we use RGBA.
+	 * @return
+	 *   This function return only one ObjFile object. 
+	 *   If the *.obj file actually defines more than two things, 
+	 *   <b>This function will treat them as a whole.</b><br/>
+	 *   There is another function that will return ObjFile[].
+	 * @throws IOException
+	 */
+	ObjFile readObjFileAsAWhole(String assetsName, int colorDataSize) throws IOException {
+		long time=SystemClock.uptimeMillis();
+		
 		InputStream istream=res.getAssets().open(assetsName);
 		Scanner scan=new Scanner(istream);
 		ObjFile ans=new ObjFile();
-		ArrayList<Float> _vertex=new ArrayList<Float>();
-		ArrayList<Short> _element=new ArrayList<Short>();
-		ArrayList<Float> _normal=new ArrayList<Float>();
-		ans.fCount=0;
-		while(true){
-			showLoadingWait();
-			if(!scan.hasNextLine()) break;
-			String line=scan.nextLine();
-			StringBufferInputStream sistream= new StringBufferInputStream(line);
-			Scanner scan2=new Scanner(sistream);
-			String lineHeader=scan2.next();
-			if(lineHeader.compareTo("v")==0) {
-				_vertex.add(scan2.nextFloat());
-				_vertex.add(scan2.nextFloat());
-				_vertex.add(scan2.nextFloat());
-			} else if(lineHeader.compareTo("f")==0) {ans.fCount++;};
-			scan2.close();
-		}
-		ans.vCount=_vertex.size();
-		ans.vertices=ByteBuffer.allocateDirect(ans.fCount*3*3*mBytesPerFloat).order(ByteOrder.nativeOrder()).asFloatBuffer();
-		ans.colors=ByteBuffer.allocateDirect(ans.fCount*3*4*mBytesPerFloat).order(ByteOrder.nativeOrder()).asFloatBuffer();
-		ans.vertices.position(0);
-		ans.colors.position(0);
-		scan.close();
+		ArrayList<Float> vertex=new ArrayList<Float>();
+		ArrayList<Float> uv=new ArrayList<Float>();
+		ArrayList<Float> ansVertices=new ArrayList<Float>();
+		ArrayList<Float> ansTextureUVs=new ArrayList<Float>();
 		
-		istream=res.getAssets().open(assetsName);
-		scan=new Scanner(istream);
-		while(true){
+		while(scan.hasNextLine()) {
 			showLoadingWait();
-			if(!scan.hasNextLine()) break;
-			String line=scan.nextLine();
-			line=line.replace("//", "/0/").replace('/', ' ');
-			StringBufferInputStream sistream = new StringBufferInputStream(line);
-			Scanner scan2= new Scanner(sistream);
-			String lineHeader=scan2.next();
-			if(lineHeader.compareTo("f")==0) {
-
-				int index,tmp;
+			final String line=scan.nextLine().replace("//", "/0/").replace('/', ' ');
+			final String[] split=line.split(" ");
+			if(split[0].equals("v")) {
+				vertex.add(Float.parseFloat(split[1]));
+				vertex.add(Float.parseFloat(split[2]));
+				vertex.add(Float.parseFloat(split[3]));
+			}
+			if(split[0].equals("vt")) {
+				uv.add(Float.parseFloat(split[1]));
+				uv.add(Float.parseFloat(split[2]));
+			}
+			if(split[0].equals("f")) {			//We can mix 'f' together with 'v' and 'vt', because in the actual file, for every single object, any 'v' or 'vt' always appears earliers than any 'f'.
+				ans.fCount++;
 				for(int i=0;i<3;i++) {
-					if(!scan2.hasNextInt()) throw new RuntimeException("Wrong Format!");
-					index=scan2.nextInt();
-					if(!scan2.hasNextInt()) throw new RuntimeException("Wrong Format!");
-					tmp=scan2.nextInt();
-					if(!scan2.hasNextInt()) throw new RuntimeException("Wrong Format!");
-					tmp=scan2.nextInt();
-					
-					index--;
-					for(int j=index*3;j<index*3+3;j++) {
-						ans.vertices.put(_vertex.get(j));
+					final int vertexIndex=Integer.parseInt(split[1+i*3]) - 1;	// MINUS ONE !!!
+					for(int j=vertexIndex*3;j<vertexIndex*3+3;j++)
+						ansVertices.add(vertex.get(j));
+					final int uvIndex=Integer.parseInt(split[2+i*3]) - 1;		// MINUS ONE !!!
+					if(uvIndex>=0) {
+						ans.hasTextureUV=true;
+						for(int j=uvIndex*2;j<uvIndex*2+2;j++)
+							ansTextureUVs.add(uv.get(j));
 					}
+					Integer.parseInt(split[3+i*3]);
 				}
 			}
-		}
+		} scan.close();
+		
+		ans.textureUVs=ByteBuffer.allocateDirect(ans.fCount*3*2*mBytesPerFloat).order(ByteOrder.nativeOrder()).asFloatBuffer();
+		ans.vertices=ByteBuffer.allocateDirect(ans.fCount*3*3*mBytesPerFloat).order(ByteOrder.nativeOrder()).asFloatBuffer();
+		ans.colors=ByteBuffer.allocateDirect(ans.fCount*3*4*mBytesPerFloat).order(ByteOrder.nativeOrder()).asFloatBuffer();
+		ans.textureUVs.position(0);
 		ans.vertices.position(0);
+		ans.colors.position(0);
+		ans.vCount=vertex.size();
+		ans.mColorDataSize=colorDataSize;
+		ans.name=assetsName;
 		
 		for(int i=0;i<ans.fCount*3;i++){
-
 			showLoadingWait();
+			ans.vertices.put(ansVertices.get(i*3));
+			ans.vertices.put(ansVertices.get(i*3+1));
+			ans.vertices.put(ansVertices.get(i*3+2));
+			if(ans.hasTextureUV) ans.textureUVs.put(ansTextureUVs.get(i*2));
+			if(ans.hasTextureUV) ans.textureUVs.put(ansTextureUVs.get(i*2+1));
 			ans.colors.put((float)Math.random());
 			ans.colors.put((float)Math.random());
 			ans.colors.put((float)Math.random());
-			ans.colors.put(1.0f);
+			if(colorDataSize==4) ans.colors.put(1.0f);
 		}
-		ans.colors.position(0);
-
 		ans.vertices.position(0);
-		scan.close();
-		
+		ans.textureUVs.position(0);
+		ans.colors.position(0);
+		Log.d("readObjAsAWhole","Load " + assetsName + " consumed "+(SystemClock.uptimeMillis()-time) + " ms.");
 		return ans;
 	}
+	
+
+	/**
+	 * @param assetsName
+	 * @param colorDataSize
+	 *   This value can be 3 or 4.<br/>
+	 *   If it's 3, we use RGB, or we use RGBA.
+	 * @return
+	 *   This function return <b>multiple ObjFile objects</b>, 
+	 *   each of which represents every different models defined in the *.obj file.<br/>
+	 *   There is another function that will treat them as a whole and return only one ObjFile.
+	 * @throws IOException
+	 */
+	ObjFile[] readObjFileToArr(String assetsName, int colorDataSize) throws IOException {
+		long time=SystemClock.uptimeMillis();
+		
+		InputStream istream=res.getAssets().open(assetsName);
+		Scanner scan=new Scanner(istream);
+		ObjFile currentObj=null;
+		ArrayList<ObjFile> ans=new ArrayList<ObjFile>();
+		ArrayList<Float> vertex=new ArrayList<Float>();
+		ArrayList<Float> uv=new ArrayList<Float>();
+		ArrayList<Float> ansVertices=new ArrayList<Float>();
+		ArrayList<Float> ansTextureUVs=new ArrayList<Float>();
+		int currentAnsIndex=0;
+		
+		while(scan.hasNextLine()) {
+			showLoadingWait();
+			final String line=scan.nextLine().replace("//", "/0/").replace('/', ' ');
+			final String[] split=line.split(" ");
+			if(split[0].equals("o")) {
+				currentObj=new ObjFile();
+				ans.add(currentObj);
+				if(split.length>1) currentObj.name=split[1];
+			}
+			if(split[0].equals("v")) {
+				vertex.add(Float.parseFloat(split[1]));
+				vertex.add(Float.parseFloat(split[2]));
+				vertex.add(Float.parseFloat(split[3]));
+			}
+			if(split[0].equals("vt")) {
+				uv.add(Float.parseFloat(split[1]));
+				uv.add(Float.parseFloat(split[2]));
+			}
+			if(split[0].equals("f")) {			//We can mix 'f' together with 'v' and 'vt', because in the actual file, for every single object, any 'v' or 'vt' always appears earliers than any 'f'.
+				currentObj.fCount++;
+				for(int i=0;i<3;i++) {
+					final int vertexIndex=Integer.parseInt(split[1+i*3]) - 1;	// MINUS ONE !!!
+					for(int j=vertexIndex*3;j<vertexIndex*3+3;j++)
+						ansVertices.add(vertex.get(j));
+					final int uvIndex=Integer.parseInt(split[2+i*3]) - 1;		// MINUS ONE !!!
+					if(uvIndex>=0) {
+						currentObj.hasTextureUV=true;
+						for(int j=uvIndex*2;j<uvIndex*2+2;j++)
+							ansTextureUVs.add(uv.get(j));
+					}
+					Integer.parseInt(split[3+i*3]);
+				}
+			}
+		} scan.close();
+		
+		for(int k=0;k<ans.size();k++) {
+			ObjFile cObj=ans.get(k);
+			cObj.textureUVs=ByteBuffer.allocateDirect(cObj.fCount*3*2*mBytesPerFloat).order(ByteOrder.nativeOrder()).asFloatBuffer();
+			cObj.vertices=ByteBuffer.allocateDirect(cObj.fCount*3*3*mBytesPerFloat).order(ByteOrder.nativeOrder()).asFloatBuffer();
+			cObj.colors=ByteBuffer.allocateDirect(cObj.fCount*3*4*mBytesPerFloat).order(ByteOrder.nativeOrder()).asFloatBuffer();
+			cObj.textureUVs.position(0);
+			cObj.vertices.position(0);
+			cObj.colors.position(0);
+			cObj.vCount=vertex.size();
+			cObj.mColorDataSize=colorDataSize;
+			final int tmp=currentAnsIndex;
+			for(;currentAnsIndex<tmp+cObj.fCount*3;currentAnsIndex++){
+				showLoadingWait();
+				cObj.vertices.put(ansVertices.get(currentAnsIndex*3));
+				cObj.vertices.put(ansVertices.get(currentAnsIndex*3+1));
+				cObj.vertices.put(ansVertices.get(currentAnsIndex*3+2));
+				if(cObj.hasTextureUV) cObj.textureUVs.put(ansTextureUVs.get(currentAnsIndex*2));
+				if(cObj.hasTextureUV) cObj.textureUVs.put(ansTextureUVs.get(currentAnsIndex*2+1));
+				cObj.colors.put((float)Math.random());
+				cObj.colors.put((float)Math.random());
+				cObj.colors.put((float)Math.random());
+				if(colorDataSize==4) cObj.colors.put(1.0f);
+			}
+			cObj.vertices.position(0);
+			cObj.textureUVs.position(0);
+			cObj.colors.position(0);
+			Log.d("readObjToArr",cObj.name);
+		}
+		Log.d("readObjToArr","Load " + assetsName + " consumed "+(SystemClock.uptimeMillis()-time) + " ms.");
+		return ans.toArray(emptyObjFileArray);
+	}
+	
+	final ObjFile[] emptyObjFileArray=new ObjFile[]{};
+	
+	private Runnable mShowLoadingWait=new Runnable(){
+		public void run(){
+			mOverlayView.show3DToast("Loading Model, Please wait.\nPlease keep your phone vertical to the ground.");	
+		}
+	};
 	
 	void showLoadingWait()
 	{
-		mDad.runOnUiThread(new Runnable(){
-			public void run(){
-				mOverlayView.show3DToast("Loading Model, Please wait.\nPlease keep your phone vertical to the ground.");	
-			}
-		});
+		mDad.runOnUiThread(mShowLoadingWait);
 	}
-
-	/*ObjFile readCompleteVerticesFromObjFile(String assetsName) throws IOException{
-		InputStream istream=res.getAssets().open(assetsName);
-		Scanner scan=new Scanner(istream);
-		ObjFile ans=new ObjFile();
-		ArrayList<Float> _vertex=new ArrayList<Float>();
-		ArrayList<Short> _element=new ArrayList<Short>();
-		ArrayList<Float> _normal=new ArrayList<Float>();
-		ans.fCount=0;
-		while(true){
-			if(!scan.hasNextLine()) break;
-			String line=scan.nextLine();
-			StringBufferInputStream sistream= new StringBufferInputStream(line);
-			Scanner scan2=new Scanner(sistream);
-			String lineHeader=scan2.next();
-			if(lineHeader.compareTo("v")==0) {
-				_vertex.add(scan2.nextFloat());
-				_vertex.add(scan2.nextFloat());
-				_vertex.add(scan2.nextFloat());
-			} else if(lineHeader.compareTo("f")==0) {ans.fCount++;};
-			scan2.close();
-		}
-		ans.vCount=_vertex.size();
-		ans.vertices=ByteBuffer.allocateDirect(ans.fCount*3*3*mBytesPerFloat).order(ByteOrder.nativeOrder()).asFloatBuffer();
-		ans.colors=ByteBuffer.allocateDirect(ans.fCount*4*mBytesPerFloat).order(ByteOrder.nativeOrder()).asFloatBuffer();
-		ans.vertices.position(0);
-		ans.colors.position(0);
-		scan.close();
-		
-		istream=res.getAssets().open(assetsName);
-		scan=new Scanner(istream);
-		while(true){
-			if(!scan.hasNextLine()) break;
-			String line=scan.nextLine();
-			line=line.replace('/', ' ');
-			StringBufferInputStream sistream = new StringBufferInputStream(line);
-			Scanner scan2= new Scanner(sistream);
-			String lineHeader=scan2.next();
-			//scan2.useDelimiter("[/\\s]");
-			if(lineHeader.compareTo("f")==0) {
-
-				int index,tmp;
-				for(int i=0;i<3;i++) {
-					if(!scan2.hasNextInt()) throw new RuntimeException("Wrong Format!");
-					index=scan2.nextInt();
-					if(!scan2.hasNextInt()) throw new RuntimeException("Wrong Format!");
-					tmp=scan2.nextInt();
-					
-					index--;
-					for(int j=index*3;j<index*3+3;j++) {
-						ans.vertices.put(_vertex.get(j));
-					}
-				}
-			}
-		}
-		ans.vertices.position(0);
-		
-		for(int i=0;i<ans.fCount;i++){
-			ans.colors.put((float)Math.random());
-			ans.colors.put((float)Math.random());
-			ans.colors.put((float)Math.random());
-			ans.colors.put(1.0f);
-		}
-		ans.colors.position(0);
-		
-		return ans;
-	}*/
 }
