e5ec1e184660f943926e83cd3e3f7df3d561a8a0
子张 <zxu32@illinois.edu>
2014-11-01 21:53:18 -0500

    rotate

--- src/com/IYYX/cardboard/myAPIs/RotateAnimator.java
+++ src/com/IYYX/cardboard/myAPIs/RotateAnimator.java
@@ -1,13 +1,45 @@
 package com.IYYX.cardboard.myAPIs;
 
+import android.opengl.Matrix;
+
 public abstract class RotateAnimator {
 	final GameObject mObj;
+	float[] mAxis;
+	float mAngularSpeed;
+	long mDurationMilliseconds;
+	boolean pause=true;
+	long lastTime;
 	public RotateAnimator(GameObject obj) {mObj=obj;}
-	public abstract void setAnimation(float[] axis,float angularSpeed);
-	public abstract void startAnimation(long durationMilliseconds);
-	public abstract void pauseAnimation();
-	public abstract void resumeAnimation();
-	public abstract void stopAndResetAnimation();
+	public void setAnimation(float[] axis,float angularSpeed){
+		mAxis=axis;
+		mAngularSpeed=angularSpeed;
+	}
 	
-	public abstract void doAnimation();
+	public  void startAnimation(long durationMilliseconds){
+		lastTime=System.currentTimeMillis();
+		mDurationMilliseconds=durationMilliseconds;
+	}
+	public void pauseAnimation(){
+		pause=true;
+	}
+	public void resumeAnimation(){
+		pause=false;
+	}
+	public void stopAndResetAnimation(){
+		pause=true;
+		mAxis=null;
+		mAngularSpeed=0f;
+		mDurationMilliseconds=0;
+	}
+	
+	public void doAnimation(){
+		//Matrix.rotateM(mObj.mModelMatrix, 0, a, x, y, z);
+		//Matrix.rotateM(answer, 0, readonlyMatrix, 0, a, x, y, z);
+		if (System.currentTimeMillis()>mDurationMilliseconds)
+			stopAndResetAnimation();
+		else
+			Matrix.rotateM(mObj.mModelMatrix, 0, mAngularSpeed*((System.currentTimeMillis()-lastTime)/1000L), mAxis[0],mAxis[1],mAxis[2]);
+		
+		
+	}
 }
