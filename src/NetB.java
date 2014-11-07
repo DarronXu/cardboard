091558474b12317435c7bc16b5e5ff91e803c003
Dave Guo <yguo42@illinois.edu>
2014-11-06 19:12:39 -0600

    TCP_Client-1.0

---
+++ src/NetB.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.*;
+import java.util.Scanner;
+public class NetB {
+
+	public static void main(String[] args) {
+		Scanner stdin=new Scanner(System.in);
+		System.out.print("Please input your name: ");
+		String clientName = stdin.next();
+		System.out.print("Please input server IP or Hostname: ");
+		String serverIP=stdin.next();
+		System.out.print("Please input server Port: ");
+		int port=stdin.nextInt();
+		Socket socket;
+		try {
+			socket=new Socket(serverIP,port);
+		} catch (Exception e) {
+			System.err.println("Failed to connect to server.");
+			e.printStackTrace();
+			return;
+		}
+		OutputStream out;
+		InputStream in;
+		try {
+			out=socket.getOutputStream();
+			in=socket.getInputStream();
+		} catch (IOException e) {
+			System.err.println("Failed to create OutputStream from socket.");
+			e.printStackTrace();
+			return;
+		}
+		OutputStreamWriter writer=new OutputStreamWriter(out);
+		Scanner reader = new Scanner(in);
+		try {
+			writer.write(clientName+"\n");
+			writer.flush();
+			String listener = stdin.next();
+			writer.write(listener + "\n");
+			writer.flush();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		/*
+		 * Important reading from server.
+		 */
+		try {
+			socket.setSoTimeout(50);
+		} catch (SocketException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		while(true)
+		{
+			try {
+				if (reader.hasNextLine()) reader.nextLine();
+				String str=stdin.nextLine();
+				writer.write(str+System.lineSeparator());
+				writer.flush();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	} 
+
+}
